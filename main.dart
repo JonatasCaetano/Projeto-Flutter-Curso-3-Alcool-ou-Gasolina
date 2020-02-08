ae75b5847dcd57e726813e87db5cf055c9b58036
Jonatas Caetano <59484665+JonatasCaetano@users.noreply.github.com>
2020-02-07 22:41:58 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'AlcoolOuGasolina.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: AlcoolOuGasolina(
+
+  ),
+)
+);
+
